153f012e17f273f276f539bac58fdb0cd03b7891
Charles Kasina <ckasina@outlook.com>
2024-01-03 22:47:09 +0000

    Inital styles setup with bg image + site-wide font

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,9 @@
+import "./styles/main.scss";
+import CloudsBG from "./assets/cloudsbg.png";
+
 function App() {
   return (
-    <div className="App">
+    <div style={{ backgroundImage: `url(${CloudsBG})` }} className="App">
       <h1>Initial React Setup</h1>
     </div>
   );
